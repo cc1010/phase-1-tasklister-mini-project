bf883b38fd8ceaac553df41ddff0483353d39a34
chaim <chaimyisroel@gmail.com>
2021-08-14 22:25:11 -0400

    second

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('#create-task-form').addEventListener('submit', ()=> {
+    preventDefault()
+  }, false)
+
+
+  const clearList = document.getElementById('tasks')
+
+document.getElementById('clearBTN').addEventListener('click', () => {
+  clearList.remove()
+})
+  
 });
+
